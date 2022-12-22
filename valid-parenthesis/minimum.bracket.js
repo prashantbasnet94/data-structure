68061d3ed9c0031e79d40c714b9491329d70a360
prashant.basnet@selu.edu <prashant.basnet@selu.edu>
2022-12-22 17:37:36 -0600

    parenthesis

--- valid-parenthesis/minimum.bracket.js
+++ valid-parenthesis/minimum.bracket.js
@@ -20,7 +20,32 @@
 // }
 
 
+/*
 
+import java.io.*;
+import java.util.*;
+
+class Solution {
+
+  static int[] getIndicesOfItemWeights(int[] arr, int limit) {
+    // your code goes here
+    HashMap<Integer,Integer> map = new HashMap<>();
+    for(int i = 0;i < arr.length;i++){
+      if(map.containsKey(limit-arr[i])){
+        return new int[]{i,map.get(limit-arr[i])};
+      }
+      map.put(arr[i],i);
+    }
+    return new int[2];
+  }
+
+  public static void main(String[] args) {
+      
+  }
+
+}
+
+*/
 
 function getIndicesOfItemWeights(arr, limit) {
     // your code goes here
